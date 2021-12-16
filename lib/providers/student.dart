86133bb3b2a6999b4c896eee7890d2f6158a4bbb
naveenkmavoor <naveenkmavoor1@gmail.com>
2021-12-16 17:19:13 +0530

    feat(student.dart): Error handling

--- lib/providers/student.dart
+++ lib/providers/student.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -21,28 +22,42 @@ class Students with ChangeNotifier {
     return [..._students];
   }
 
-  Future<void> fetchStudents() async {
-    final http.Response response = await http.get(
-      Uri.parse(
-          'https://hamon-interviewapi.herokuapp.com/students/?api_key=8A46f'),
-    );
-    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+  Future<void> getStudents() async {
+    const url =
+        'https://hamon-interviewapi.herokuapp.com/students/?api_key=8A46f';
 
-    final loadedStudents = (extractedData['students'] as List<dynamic>)
-        .map((student) => Student(
-            id: student['id'],
-            name: student['name'],
-            age: student['age'],
-            email: student['email']))
-        .toList();
+    try {
+      final http.Response response = await http
+          .get(
+            Uri.parse(url),
+          )
+          .timeout(const Duration(seconds: 10));
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
 
-    _students = loadedStudents;
+      final loadedStudents = (extractedData['students'] as List<dynamic>)
+          .map((student) => Student(
+              id: student['id'],
+              name: student['name'],
+              age: student['age'],
+              email: student['email']))
+          .toList();
+
+      _students = loadedStudents;
+    } catch (err) {
+      rethrow;
+    }
   }
 
   Future<Map<String, dynamic>> fetchStudentById(int id) async {
-    final http.Response response = await http.get(Uri.parse(
-        'https://hamon-interviewapi.herokuapp.com/students/${id}?api_key=8A46f'));
-    final loadedStudent = json.decode(response.body) as Map<String, dynamic>;
-    return loadedStudent;
+    try {
+      final http.Response response = await http
+          .get(Uri.parse(
+              'https://hamon-interviewapi.herokuapp.com/students/$id?api_key=8A46f'))
+          .timeout(const Duration(seconds: 10));
+      final loadedStudent = json.decode(response.body) as Map<String, dynamic>;
+      return loadedStudent;
+    } catch (err) {
+      rethrow;
+    }
   }
 }
